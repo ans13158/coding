05ec5214eb833ab38c7f3f0888221c020f5bfff3
anshul agrawal <anshulagrawal455@gmail.com>
2017-11-01 17:18:07 +0530

    #13. 1/11/2017. Added bookmark feature; PROBLEM : Bookmark images for all questions changing whenever any question is bookmarked. Solve it if you canclearclear!

--- disp_mcq.php
+++ disp_mcq.php
@@ -142,7 +142,7 @@
 
 	var question = parseInt(urlParam('k'));
 
-	
+
 </script>
 
 <div class="page-wrap">
@@ -182,7 +182,7 @@
             <div class="mcq-ques">
                 <div class="col-md-1 ques-heading" >
                     <h4> <?= $quesNo ?>.</h4>
-                    <img src="assets/images/bookmark_before.svg" style="margin-left: -10px;" id="review_ques">
+                    <img src="assets/images/bookmark_before.svg" style="margin-left: -10px;" id="review_ques<?= $quesNo ?>">
                     <span id="bookmark-dialog"><p>Mark Question</p></span>
                 </div>
                 <div class="col-md-11 ques-content">
@@ -263,224 +263,187 @@
 </div>	
 
 <script>
+    var reviewId = "review_ques" + (question - 100);
 
-    var review_img = document.getElementById("review_ques");
-    var clickCount = 0
-    var bookmark_dialog = document.getElementById("bookmark-dialog");
-    review_img.onmouseover = function()  {
-        bookmark_dialog.style = "display:block";
-    }
-    review_img.onmouseout = function()  {
-        bookmark_dialog.style = "display:none";
-    }
-    // review_img.onmouseover = function()  {
-    //     review_img.src = "assets/images/bookmark_after.png";
-    //     review_img.style = "height :40px;width:40px"
-    // }
-
-    // review_img.onmouseout = function()  {
-    //     review_img.src="assets/images/bookmark_before.svg";
-    // }
-
-    review_img.onclick = function()  {
-         clickCount++;
-         if(clickCount %2 == 1)  {
-             review_img.src = "assets/images/bookmark_after.png";
-             review_img.style = "height :40px;width:40px";  
-         }
-         else
-            review_img.src="assets/images/bookmark_before.svg";
-    }
-
-	function check(optNo)  {
-		
-	 	/* Section enables checking only 1 checkbox at a time. */
-
-	 	/*REMOVED FOR NOW*/
-
-
-	 		// if(document.getElementById(optNo).checked == true)  {
-		 	// 	document.getElementById(optNo).checked = false;
-		 	// }
-		 	// for(var i = 1; i <=4; i++)
-		 	// 	document.getElementById(i).checked = false;
-
-		 	// document.getElementById(optNo).checked = true;
-
-	 	/* Section enables checking only 1 checkbox at a time. */
-
-
-	 	/*Fetch value of selected checkbox and store in "answers[]" array */
-			var checked = [];
-			var elements = document.getElementsByClassName('checkBox');
-			for(var i =0; elements[i]; i++)  {
-				//alert(elements[i].checked)
-				if(elements[i].checked){
-					checked[i] = elements[i].value;
-                    alert(elements[i].value)
-
-                }
-				else
-					checked[i] = 0;	 
-			}
-            
-			for(var i = 0; i<4; i++)  {
-				if(checked[i] != '0' || checked[i] != 0)   {
-					answers[question-101]  = parseInt(checked[i]);
-                    //alert(answers)
-                }
-			}	
-	 	/*Fetch value of selected checkbox and store in "answers[]" array */
-
-	 	responses = JSON.stringify(answers);
-	 	document.cookie = 'responses=' + responses;
+//Image used for bookmark icon
+var review_img = document.getElementById(reviewId);
+//Counts no. of clicks on bookmark icon. For odd clicks, bookmark ques.; else unmark
+var clickCount = 0
+    //Dialog "Mark Question" appears when bookmark icon is hovered upon.
+var bookmark_dialog = document.getElementById("bookmark-dialog");
+review_img.onmouseover = function() {
+    bookmark_dialog.style = "display:block";
+}
+review_img.onmouseout = function() {
+    bookmark_dialog.style = "display:none";
 }
 
-function markReview(questionNo)  {
-	 review[questionNo-101] = true;
-	 changeReview();
-	 document.cookie = 'review=' + JSON.stringify(review);
+/*
+ * Change image when bookmark icon clicked.
+ * For odd no. of clicks : bookmark_after icon else bookmark_before
+ */
+review_img.onclick = function() {
+    clickCount++;
+    alert("clicks : " + clickCount);
+    if (clickCount % 2 == 1) {
+        review_img.src = "assets/images/bookmark_after.png";
+        review_img.style = "height :40px;width:40px";
+        review[question - 101] = 1;
+        document.cookie = "review = " + JSON.stringify(review);
+        changeReviewColor();
+    } else {
+        review_img.src = "assets/images/bookmark_before.svg";
+        review[question - 101] = 0;
+        document.cookie = "review = " + JSON.stringify(review);
+        changeReviewColor();
+    }
+
 }
-	
-	/* 
-	 * "no_of_ques" stores no. of questions  in db.	
-	 * "question" stores the current question no.
-	 * "prevQues()" & "nextQues()" give functionalities to PREVIOUS & NEXT buttons.
-	 */
 
-	var no_of_ques = document.getElementById('max_question').value ;
-	no_of_ques = parseInt(no_of_ques);
-	no_of_ques += 100;
+function check(optNo) {
 
-	var previous = document.getElementById('previous_btn');
-	var next = document.getElementById('next_btn');
-	
-	/*
-	* Disable NEXT or PREVIOUS buttons a/c to current ques no. :
-	* If current ques. is first question in list, disable PREVIOUS.
-	* If ques. last in list, disable NEXT.
-	*/
-	if(question == no_of_ques )  {
-		next.disabled = true;
-	}
-	if(question == 101)
-		previous.disabled = true;
+    /* Section enables checking only 1 checkbox at a time. */
 
-	function prevQues()  {
-		window.location.href = "disp_mcq.php?k=" + (question-1);
-	}
-	function nextQues()  {
-		window.location.href = "disp_mcq.php?k=" + (question+1);
-	}
+    /*REMOVED FOR NOW*/
 
-	
+    // if(document.getElementById(optNo).checked == true)  {
+    // 	document.getElementById(optNo).checked = false;
+    // }
+    // for(var i = 1; i <=4; i++)
+    // 	document.getElementById(i).checked = false;
 
-			/*
-			 * Section makes TIME LEFT section dynamic.
-			*/	
-  function checkTime(i) {
-	  if (i < 10) {
-	    i = "0" + i;
-	  }
-	  return i;
-  }
+    // document.getElementById(optNo).checked = true;
+
+    /* Section enables checking only 1 checkbox at a time. */
+
+    /*Fetch value of selected checkbox and store in "answers[]" array */
+    var checked = [];
+    var elements = document.getElementsByClassName('checkBox');
+    for (var i = 0; elements[i]; i++) {
+        //alert(elements[i].checked)
+        if (elements[i].checked) {
+            checked[i] = elements[i].value;
+            alert(elements[i].value)
+
+        } else
+            checked[i] = 0;
+    }
+
+    for (var i = 0; i < 4; i++) {
+        if (checked[i] != '0' || checked[i] != 0) {
+            answers[question - 101] = parseInt(checked[i]);
+            //alert(answers)
+        }
+    }
+    /*Fetch value of selected checkbox and store in "answers[]" array */
+
+    responses = JSON.stringify(answers);
+    document.cookie = 'responses=' + responses;
+}
+
+/* 
+ * "no_of_ques" stores no. of questions  in db.	
+ * "question" stores the current question no.
+ * "prevQues()" & "nextQues()" give functionalities to PREVIOUS & NEXT buttons.
+ */
+
+var no_of_ques = document.getElementById('max_question').value;
+no_of_ques = parseInt(no_of_ques);
+no_of_ques += 100;
+
+var previous = document.getElementById('previous_btn');
+var next = document.getElementById('next_btn');
+
+/*
+ * Disable NEXT or PREVIOUS buttons a/c to current ques no. :
+ * If current ques. is first question in list, disable PREVIOUS.
+ * If ques. last in list, disable NEXT.
+ */
+if (question == no_of_ques) {
+    next.disabled = true;
+}
+if (question == 101)
+    previous.disabled = true;
+
+function prevQues() {
+    window.location.href = "disp_mcq.php?k=" + (question - 1);
+}
+
+function nextQues() {
+    window.location.href = "disp_mcq.php?k=" + (question + 1);
+}
+
+/*
+ * Section makes TIME LEFT section dynamic.
+ */
+function checkTime(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
 
 function startTime() {
-  var today = new Date();
-  var h = today.getHours();
-  var m = today.getMinutes();
-  var s = today.getSeconds();
-  // add a zero in front of numbers<10
-  m = checkTime(m);
-  s = checkTime(s);
-  n = h + " : " + m +  " : " + s ;
-  document.getElementById('clock').innerHTML = "<h4> Time Left  " + n + "</h4>";
-  t = setTimeout(function() {
-    startTime()
-  }, 1000);
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    // add a zero in front of numbers<10
+    m = checkTime(m);
+    s = checkTime(s);
+    n = h + " : " + m + " : " + s;
+    document.getElementById('clock').innerHTML = "<h4> Time Left  " + n + "</h4>";
+    t = setTimeout(function() {
+        startTime()
+    }, 1000);
 }
 startTime();
 
-	/*
-	* Function changes colors of circles with questions that are attempted, unattempted, etc. a/c to color * code. 
-	*/
-function changeColor()  {
-        
-		for(var i = 0 ; i < answers.length; i++)  {
-			if(answers[i])  {
-			   id = "ques" + (i+1) ;
-			   document.getElementById(id).classList.remove("unattempted");
-			   document.getElementById(id).classList.add("attempted");
-			   if(question - 101 == i )  {
-                  optId = "option" + answers[i];
-			   	  document.getElementById(optId).checked = true; 
-               }
-			}
-		}
-			 //   id = "ques" + (i+1) ;
-			 //   var classes = document.getElementById(id).className.split(" ");
-			 //   for(var i =0;i<classes.length;i++)  {
-				//    if(classes[i] != "attempted" )  {
-				// 		document.getElementById(id).classList.remove("attempted");
-				//    		document.getElementById(id).classList.add("unattempted");
-				// 	}
-				// }	
-	}
+/*
+ * Function changes colors of circles with questions that are attempted, unattempted, etc. a/c to color * code. 
+ */
 
-	function changeReview()  {
-		for(var i =0; i <review.length;i++)  {
-			if( review[i] != null)  {
-				review_id = "review_btn" + (question-100);
-				var review_btn = document.getElementById(review_id);
-	 		   
-				id = "ques" + (i+1);
-				var classes = document.getElementById(id).className.split(" ");
-				for(var j=0; j<classes.length;j++)  {
-					
-					if(classes[j] == "attempted")  {
-						//document.getElementById(id).classList.remove("attempted");
-			  			document.getElementById(id).classList.add("review");
-						review_btn.innerHTML = "Unmark Review";
+function changeReviewColor() {
+    if (review.length) {
+        alert(review);
+        for (var i = 0; i < review.length; i++) {
+            var id = "ques" + (i + 1);
+            if (review[i] == 1 || review[i] == "1") {
+                document.getElementById(id).classList.add("review");
+                review_img.src = "assets/images/bookmark_after.png";
+                review_img.style = "height :40px;width:40px";
+                clickCount = 1;
+            }
+        }
+    }
+}
 
-					}
-					else if(classes[j] == "unattempted")  {
-						//document.getElementById(id).classList.remove("unattempted");
-			   			document.getElementById(id).classList.add("review");
-						review_btn.innerHTML = "Unmark Review";
+changeReviewColor();
 
-					}
-					
+function changeColor() {
 
-				}
-				document.getElementById('review_btn').innerHTML = "Unmark Review";
-				document.getElementById('review_btn').removeAttribute('onclick');
-				document.getElementById('review_btn').onclick = function()  {
-					review[k] = false;
-					review_btn.innerHTML = "Mark For Review";
-					alert(document.getElementById(id).classList);
-					document.getElementById(id).classList.remove("review");
-					alert(document.getElementById(id).classList);
-					document.cookie = JSON.stringify(review);
-					//changeColor();
-				}
-			}
-		}
-	}
+    for (var i = 0; i < answers.length; i++) {
+        if (answers[i]) {
+            id = "ques" + (i + 1);
+            document.getElementById(id).classList.remove("unattempted");
+            document.getElementById(id).classList.add("attempted");
+            if (question - 101 == i) {
+                optId = "option" + answers[i];
+                document.getElementById(optId).checked = true;
+            }
+        }
+    }
+    //   id = "ques" + (i+1) ;
+    //   var classes = document.getElementById(id).className.split(" ");
+    //   for(var i =0;i<classes.length;i++)  {
+    //    if(classes[i] != "attempted" )  {
+    // 		document.getElementById(id).classList.remove("attempted");
+    //    		document.getElementById(id).classList.add("unattempted");
+    // 	}
+    // }	
+}
 
-	// function unMark_review(k)  {
-	// 	//alert('here');
-	// 	review[k] = 0;
-	// 	review_btn = document.getElementById('review_btn');
-
-	// 	review_btn.innerHTML = "Mark For Review";
-	// 	document.getElementById(id).classList.remove("review");
-	// 	changeColor();
-	// 	//review_btn.onclick = changeReview();
-	// 	return;
-	// }
-
-	changeColor();
-	changeReview();
-
+changeColor();
+// changeReview();
 
 </script>
